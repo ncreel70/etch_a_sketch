5a7ef6d510f04b56f570045933f6a01685a26227
Noah Creel <noah.creel@gmail.com>
2020-02-16 21:18:37 -0700

    Fixed wrapping issue with rows, centered grid horizontally

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-// let gridSize = document.getElementById('gridSize').value;
 
 document.addEventListener('click',() => {
    
    let container = document.getElementById('container');
-   let gridSize = 16;
+   let gridSize = document.getElementById('gridSize').value;
    let dimension = 960/gridSize;
    
+   
    for (let i = 0; i < (gridSize); i ++){
+      
       let divRow = document.createElement('div');
       divRow.className = 'row';
       divRow.style.height = dimension + 'px';
       
-      for (let k = 1; k <= gridSize; k++) {
+      for (let k = 0; k < (gridSize); k++) {
          let divCell = document.createElement('div');
          divCell.className = 'cell';
          divCell.id = 'cell' + k + i;
          divRow.appendChild(divCell);
          divCell.style.height = '100%';
          divCell.style.width = dimension + 'px';
-         divCell.style.float = 'left';
+         divCell.style.gridAutoColumns = 'auto';
          divCell.onmouseover = mouseHoverColorChange;
       } 
       
